3a3569cf21a92a2f7132829c63190056145b099a
Erol Kahraman <69480343+kahramaneron@users.noreply.github.com>
2020-12-09 23:17:31 +0300

    Update soru_7.py

--- soru_7.py
+++ soru_7.py
@@ -1,27 +1,26 @@
-#BINEARY SEARCH ALGORITHM
+#BINEARY SEARCH ALGORITHM with RECURSIVE
 
 def binearySearch(dizi,istenen,sol,sag):    #fonksiyonumuzu olusturduk
    
    
     varMi=False #istenen sayiyi kontrol edecegiz
 
-    if sol<=sag:#ortancanin solunda küçük sayilar bulunuyor, saginda ise büyük sayilar. sol, sagdan büyük oldugu surece ve
-        return -1                       #istenen sayi bulunmadigi sürece devam edecek
-
+    while sol<=sag and not varMi:   #ortancanin solunda küçük sayilar bulunuyor, saginda ise büyük sayilar. sol, sagdan büyük oldugu surece ve
+                                    #istenen sayi bulunmadigi sürece devam edecek
         ortanca = (sol+sag)//2      #diziyi ortadan bölüp divide and conquere mantigi ile hareket ediyoruz.
 
         if dizi[ortanca] > istenen: #ortanca sayi aranilan sayidan büyük oldugu durumda
             return binearySearch(dizi,istenen,sol,ortanca-1)     #sagdaki sayi ortadan bir büyük olacak ve bununla birlikte döngüde büyük olan kisimlar isleme girmeyecek.
 
-        if dizi[ortanca] < istenen:   #ortadaki sayi aranilan sayidan küçükse
+        elif dizi[ortanca] < istenen:   #ortadaki sayi aranilan sayidan küçükse
             return binearySearch(dizi,istenen,ortanca+1,sag)     #bastaki sayi ortancadan bir büyük olarak tanimladik
                                 #küçük olan kisimlar devre disi kalacak
         else:
             varMi=True  #eger ki aradigimiz sayi ortada ise boolean degerimiz True ya dönecek
 
     return varMi
-
-dizi=[1]        #eleman sayisi 1 
+#----------------->DİZİ 1 ELEMANLI
+dizi=[1]        
 aranilan=8
 sol=0
 sag=len(dizi)-1
@@ -34,7 +33,9 @@ if inIt:
 else:
     print("Istenilen sayi dizi içerisinde yoktur.")
 
-dizi=[1,5,7,4,2,3,9]        #eleman sayisi tek
+#----------------->DİZİ ELEMAN SAYISI TEK
+
+dizi=[1,5,7,4,2,3,9]       
 aranilan=8
 sol=0
 sag=len(dizi)-1
@@ -46,6 +47,8 @@ if inIt:
 
 else:
     print("Istenilen sayi dizi içerisinde yoktur.")
+      
+#----------------->DİZİ ELEMAN SAYISI ÇİFT
 
 dizi=[1,8,2,4]        #eleman sayisi çift
 aranilan=8
@@ -59,8 +62,5 @@ if inIt:
 
 else:
     print("Istenilen sayi dizi içerisinde yoktur.")
-
-
-
 
         
